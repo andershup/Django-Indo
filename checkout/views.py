21d551c4992670a4b71ea0d57e337a1a8f053dab
andershup <anders242@gmail.com>
2020-09-21 08:53:18 +0000

    Card payment card added

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HTj2cCi7YLtVGm0pWL4D5Q5sk08Gh2knTN9EGqcxOhDfs9TAqwWi3Cle4HHsDyQoOg843xbuar1vub2G810qMCQ00s6p7kIym',
+        'test_client_secret': 'test client secret',
     }
 
     return render(request, template, context)
