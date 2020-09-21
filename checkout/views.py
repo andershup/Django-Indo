f1d8397d3d0558d2e317d3afd603a1536ad4bb98
andershup <anders242@gmail.com>
2020-09-21 11:37:40 +0000

    dfjskfsdf

--- checkout/views.py
+++ checkout/views.py
@@ -39,7 +39,7 @@ def checkout(request):
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'test_client_secret': intent.test_client_secret,
+        'test_client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
